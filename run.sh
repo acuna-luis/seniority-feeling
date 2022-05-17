2ca90f279d89067b5b4bee1298ceb84146443d05
Luis Acuna <leacunavega@gmail.com>
2022-05-17 20:12:44 +0200

    commit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 mvn clean package -Dmaven.test.skip=true
 docker build -t luisacuna/db-spring .
-docker push luisacuna/db-spring
+docker push luisacuna/td-spring
 git add *
 git commit -m "commit"
 git push
